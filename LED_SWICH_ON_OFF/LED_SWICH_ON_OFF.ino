c859731c87b93b51c3f6700a5bc7a45a5a2b7291
Kiehyun Kevin Park <guitar79@naver.com>
2017-04-11 08:06:20 +0900

    Add files via upload

---
+++ LED_SWICH_ON_OFF/LED_SWICH_ON_OFF.ino
@@ -0,0 +1,21 @@
+void setup()
+{
+pinMode(6, INPUT_PULLUP);
+pinMode(13, 1);
+//pinMode(11, OUTPUT);
+//pinMode(10, OUTPUT);
+}
+void loop() {
+  int state;
+  state = digitalRead(6);
+  digitalWrite(13, state);
+  delay(1);
+}
+//  digitalWrite(12, 1) ;   digitalWrite(13, 0) ;   digitalWrite(11, 0) ;   digitalWrite(10, 1) ; 
+//  delay(1000);
+//  digitalWrite(12, 1) ;   digitalWrite(13, 1) ;   digitalWrite(11, 0) ;   digitalWrite(10, 0) ; 
+//  delay(1000);
+//  digitalWrite(12, 1) ;   digitalWrite(13, 0) ;   digitalWrite(11, 1) ;   digitalWrite(10, 0) ; 
+//  delay(1000);
+//  digitalWrite(12, 1) ;   digitalWrite(13, 0) ;   digitalWrite(11, 0) ;   digitalWrite(10, 0) ; 
+//  delay(1000);
