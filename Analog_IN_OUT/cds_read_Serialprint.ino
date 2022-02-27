75d36e888c089fcacd02a65a99bcdedb867a92c8
Kiehyun Kevin Park <guitar79@gs.hs.kr>
2022-02-27 20:08:46 +0900

    update

--- Analog_IN_OUT/cds_read_Serialprint.ino
+++ Analog_IN_OUT/cds_read_Serialprint.ino
@@ -12,3 +12,19 @@ void loop()
 	Serial.println(val);
 	delay(10);//delay 0.01 s
 }
+
+
+
+int led = 13;
+void setup() 
+	{                
+		pinMode(led, OUTPUT);  // 13번 핀을 출력(OUTPUT)으로 함  
+	}
+void loop() 
+	{
+		digitalWrite(led, HIGH); // 13번 핀에 5V를 인가함 (HIGH)
+		delay(1000);             // 1초 (1000ms)간 상태를 유지함
+		digitalWrite(led, LOW);  // 13번 핀에 0V를 인가함 (LOW)
+		delay(1000);             // 1초 (1000ms)간 상태를 유지함
+	}
+
