b5694bc736737dc506aa9bb0454a0fb727663807
guitar79 <guitar79@gs.hs.kr>
2022-05-19 15:13:39 +0900

    update

--- _01_Basic/serial_communication/serial_communication.ino
+++ _01_Basic/serial_communication/serial_communication.ino
@@ -1,15 +1,16 @@
 void setup(){
-Serial.begin(9600);
+    Serial.begin(9600);
 }
-  void loop(){
-  Serial.print(123);
-  Serial.print("Hello world.");
-  Serial.print("\n");
-  
-  Serial.println(456);
-  Serial.println("Hi  Everyone.");
-  Serial.println("""");
-  
-  delay(1000);
+
+void loop(){
+    Serial.print(123);
+    Serial.print("Hello world.");
+    Serial.print("\n");
+
+    Serial.println(456);
+    Serial.println("Hi  Everyone.");
+    Serial.println("""");
+
+    delay(1000);
 }
 
