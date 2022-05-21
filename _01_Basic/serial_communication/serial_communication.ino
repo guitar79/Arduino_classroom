f2879fffd541608c7e86dadb5995eee2cc98e27a
Kiehyun Kevin Park <guitar79@gs.hs.kr>
2022-05-21 09:58:16 +0900

    update

--- _01_Basic/serial_communication/serial_communication.ino
+++ _01_Basic/serial_communication/serial_communication.ino
@@ -1,15 +1,17 @@
-void setup(){
-    Serial.begin(9600);
-}
+void setup()
+    {
+        Serial.begin(9600);
+    }
 
-void loop(){
-    Serial.print(123);
-    Serial.print("Hello world.");
-    Serial.print("\n");
+void loop()
+    {
+        Serial.print(123);
+        Serial.print("Hello world.");
+        Serial.print("\n");
 
-    Serial.println(456);
-    Serial.println("Hi  Everyone.");
-    Serial.println("""");
+        Serial.println(456);
+        Serial.println("Hi  Everyone.");
+        Serial.println("""");
 
-    delay(1000);
-}
+        delay(1000);
+    }
